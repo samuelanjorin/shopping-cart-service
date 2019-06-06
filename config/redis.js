db8af8c1dd652b6c3a0a1986882d40e4dcacbc65
Samuel Anjorin <samuel.anjorin12@gmail.com>
2019-06-06 14:30:51 +0100

    set up new redis config

--- config/redis.js
+++ config/redis.js
@@ -1,14 +1,11 @@
-import redis from 'redis';
-import dotenv from "dotenv"
-dotenv.config();
+import redis from 'redis'
+import dotenv from 'dotenv'
+dotenv.config()
 
 let redisConfig = {
-    host: process.env.REDIS_HOST || '127.0.0.1',
-    port: process.env.REDIS_PORT || 6379,
-    password: process.env.REDIS_PASSWORD || '',
-    db: process.env.REDIS_DB || 'turing'
-  };
-export default redis.createClient(redisConfig.port, redisConfig.host, {
-    password: redisConfig.password //,
-  //  db:redisConfig.db
-});
+  host: process.env.REDIS_HOST || '127.0.0.1',
+  port: process.env.REDIS_PORT || 6379,
+  password: process.env.REDIS_PASSWORD || '',
+  db: process.env.REDIS_DB || 'turing'
+}
+export default redis.createClient(redisConfig.port, redisConfig.host)
